ceaf4328ad0e133d568ea1b4777c80d84cfe3cec
Leon Harvey <leonharvey@live.com>
2015-06-28 20:58:51 +0000

    update

--- cache.php
+++ cache.php
@@ -3,15 +3,39 @@
 class cache {
     
     private static $cache_store = array();
+    private $cache_file;
+    
+    public static function segment($callback, $key, $ttl = 0) {
+        $fetch = self::fetch($key);
+        
+        if (!empty($fetch)) {
+            return $fetch;
+        }
+        
+        $store = $callback();
+     
+        if (is_array($store))
+            $store = serialize($store);
+            
+        return cache::store($key, $store, $ttl);;
+    }
     
     public static function store($key, $value, $time = 0) {
+        if (!function_exists('apc_fetch')) {
+            return false;
+        }
         if (is_array($value) || is_object($value)) 
             $value = serialize($value);
 
         apc_store($key, $value, $time);
+        
+        return $value;
     }
     
     public static function fetch($key, $condition = true) {
+        if (!function_exists('apc_fetch'))
+            return false;
+            
         if ($condition == false)
             return false;
         
@@ -27,21 +51,42 @@ class cache {
         apc_delete($key);
     }
     
-    public static function fileCache($location, $cache_name = null) {
-        switch($location) {
-            case 'start': 
-                ob_start();
-            break;
-            case 'end':
-                if (!empty($cache_name)) {
-                    file_put_contents(ips_path . '/cache/' . $cache_name);
-                } 
-                return ob_get_clean();
-            break;
+    public function file($point, $cache_name = null, $fetch_only = false, $ttl = null) {
+        $this->cache_file = $this->fetchCacheFile($cache_name, $ttl);
+        
+        if ($fetch_only == true) return 'skip';
+        
+        if ($this->cache_file) {
+            echo $this->cache_file;
+            die();
+            
         }
+        
+        if ($point == 'start') {
+            ob_start();
+            return false;
+        } 
+        
+        $cache_contents = ob_get_clean();
+        
+        if (!empty($cache_name)) {
+            file_put_contents(ips_path . '/cache/' . md5($cache_name), $cache_contents);
+        } 
+        
+        return $cache_contents;
+ 
+        
     }
     
-    public static function fetchCacheFile($location) {
+    public function fetchCacheFile($cache_name, $ttl = null) {
+        $filename = ips_path . '/cache/' . md5($cache_name);
         
+        if (file_exists($filename)) {
+            
+            if ($ttl !== null && filemtime($filename) < time() - $ttl)
+                return;
+                
+            return file_get_contents($filename);
+        }
     }
 }
